fdbedc8b40cce8b77366de4b199937d4f5c50102
evren <evren@haltia.ai>
2025-01-24 13:59:24 +0300

    google api key removal

--- list_google_models.py
+++ list_google_models.py
@@ -1,7 +1,7 @@
 import google.generativeai as generativeai
 
 # Authenticate with your API key
-generativeai.auth = "AIzaSyAyvQKC3cvdVFkbczIyQnyU4bIw8mAiXUk"
+generativeai.auth = "key herek"
 
 # List available models
 models = generativeai.list_models()
